e0d87ed3572e582588f47e9f6510c065e0aa7168
Caleb Wright <wrightcl2@appstate.edu>
2022-11-26 22:49:06 -0500

    Lifted state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React from "react";
+import Header from "./Header";
+import Player from "./Player";
 import "./App.css";
 
 const App = () => {
   const [players, setPlayers] = React.useState([
     {
       name: "Caleb",
+      score: 0,
       id: 1,
     },
     {
       name: "Sari",
+      score: 0,
       id: 2,
     },
     {
       name: "Matt",
+      score: 0,
       id: 3,
     },
     {
       name: "Greg",
+      score: 0,
       id: 4,
     },
   ]);
 
   const handleRemovePlayer = (id) => {
     setPlayers((prevState) => prevState.filter((p) => p.id !== id));
+  };
+
+  const handleScoreChange = (id, delta) => {
+    setPlayers((prevState) =>
+      prevState.map((player) => {
+        if (player.id === id) {
+          return {
+            name: player.name,
+            score: player.score + delta,
+            id: player.id,
+          };
+        }
+        return player;
+      })
+    );
   };
 
   return (
@@ -32,68 +53,13 @@ const App = () => {
       {players.map((players) => (
         <Player
           name={players.name}
+          score={players.score}
           id={players.id}
           key={players.id.toString()}
           removePlayer={handleRemovePlayer}
+          changeScore={handleScoreChange}
         />
       ))}
-    </div>
-  );
-};
-
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
-    </header>
-  );
-}
-
-const Player = (props) => {
-  return (
-    <div className="player">
-      <span className="player-name">
-        <button
-          className="remove-player"
-          onClick={() => props.removePlayer(props.id)}
-        >
-          ✖
-        </button>
-        {props.name}
-      </span>
-
-      <Counter />
-    </div>
-  );
-};
-
-const Counter = () => {
-  const [score, setScore] = React.useState(0);
-
-  const incrementScore = () => {
-    setScore((prevScore) => prevScore + 1);
-  };
-
-  const decrementScore = () => {
-    setScore((prevScore) => prevScore - 1);
-  };
-
-  return (
-    <div className="counter">
-      <button
-        className="counter-action decrement"
-        onClick={() => decrementScore()}
-      >
-        -
-      </button>
-      <span className="counter-score">{score}</span>
-      <button
-        className="counter-action increment"
-        onClick={() => incrementScore()}
-      >
-        +
-      </button>
     </div>
   );
 };
